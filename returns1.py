777b5b313ab83212d4cf43e40bcbf0bb06505586
lenovo <reena20@navgurukul.org>
2021-02-19 22:49:29 +0530

    second commit

---
+++ returns1.py
@@ -0,0 +1,14 @@
+def add_numbers(number_x,number_y):
+    number_sum=number_x+number_y
+    return number_sum
+sum1=add_number(20,40)
+print(sum)
+
+sum2=add_numbers(560,23)
+a=1234
+b=12
+sum3=add_numbers(a,b)
+print(sum2)
+print(sum3)
+number_a=add_numbers(20,40)
+print(number_a)
